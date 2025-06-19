f9846207aa0f9199ba685c3cb7dd8f263d479aef
Arsenmvp <arsenklaptentko@gmail.com>
2025-06-19 09:49:08 +0300

    Create task3.js

---
+++ task3.js
@@ -0,0 +1,13 @@
+function getMaxSalary(salaries) {
+  let values = Object.values(salaries);
+  return values.length ? Math.max(...values) : 0;
+}
+
+// Приклад:
+let salaries = {
+  Anna: 300,
+  John: 500,
+  Mike: 400
+};
+
+console.log(getMaxSalary(salaries)); // 500
